300df211b104a9458d703c24489e2392c53e7089
stas <stanislav.romanikhin@lenta.com>
2023-08-11 19:21:12 +0700

    complete model

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,6 +3,7 @@ import numpy as np
 import copy
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 
 def sigmoid(z):
@@ -16,24 +17,6 @@ def sigmoid(z):
     """
 
     return 1 / (1 + np.exp(-z))
-
-
-def initialize_with_zeros(dim):
-    """
-    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
-    
-    Argument:
-    dim -- size of the w vector we want (or number of parameters in this case)
-    
-    Returns:
-    w -- initialized vector of shape (dim, 1)
-    b -- initialized scalar (corresponds to the bias) of type float
-    """
-
-    w = np.zeros((dim, 1))
-    b = 0.0
-
-    return w, b
 
 
 def propagate(w, b, X, Y):
@@ -151,63 +134,20 @@ def predict(w, b, X):
     return Y_prediction
 
 
-def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
-    """
-    Builds the logistic regression model
-    
-    Arguments:
-    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
-    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
-    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
-    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
-    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
-    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
-    print_cost -- Set to True to print the cost every 100 iterations
-    
-    Returns:
-    d -- dictionary containing information about the model.
-    """
-    
-    # initialize parameters with zeros (≈ 1 line of code)
-    w, b = initialize_with_zeros(X_train.shape[1])
-    print(w.shape)
-
-    # Gradient descent (≈ 1 line of code)
-    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
-    
-    # Retrieve parameters w and b from dictionary "parameters"
-    w = parameters["w"]
-    b = parameters["b"]
-    
-    # Predict test/train set examples (≈ 2 lines of code)
-    Y_prediction_test = predict(w, b, X_test)
-    Y_prediction_train = predict(w, b, X_train)
-
-    # Print train/test Errors
-    if print_cost:
-        print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
-        print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
-
-    
-    d = {"costs": costs,
-         "Y_prediction_test": Y_prediction_test, 
-         "Y_prediction_train" : Y_prediction_train, 
-         "w" : w, 
-         "b" : b,
-         "learning_rate" : learning_rate,
-         "num_iterations": num_iterations}
-    
-    return d
-
-
 class LogisticRegression():
 
     def __init__(self, learning_rate, num_iterations, random_state=42):
+        """
+        Argements:
+        num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
+        learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+        """
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.random_state = random_state
 
-    def initialize_with_zeros(self, dim):
+
+    def initialize_weights(self, dim):
         """
         This function creates a vector of weights of shape (dim, 1) for w and initializes b.
     
@@ -224,7 +164,8 @@ class LogisticRegression():
 
         return self
 
-    def predict(w, b, X):
+
+    def predict(self, X):
         '''
         Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
         
@@ -239,9 +180,9 @@ class LogisticRegression():
         
         m = X.shape[1]
         Y_prediction = np.zeros((1, m))
-        w = w.reshape(X.shape[0], 1)
+        self.w = self.w.reshape(X.shape[0], 1)
         
-        A =  sigmoid(np.dot(w.T, X) + b)  
+        A = sigmoid(np.dot(self.w.T, X) + self.b)  
         
         for i in range(A.shape[1]):
             
@@ -251,6 +192,87 @@ class LogisticRegression():
                 Y_prediction[0, i] = 0
     
         return Y_prediction
+
+
+    def predict_proba(self, X):
+        '''
+        Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+        
+        Arguments:
+        w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+        b -- bias, a scalar
+        X -- data of size (num_px * num_px * 3, number of examples)
+        
+        Returns:
+        Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+        '''
+        
+        m = X.shape[1]
+        Y_prediction = np.zeros((1, m))
+        self.w = self.w.reshape(X.shape[0], 1)
+        
+        A = sigmoid(np.dot(self.w.T, X) + self.b)  
+        
+        for i in range(A.shape[1]):
+            Y_prediction[0, i] = A[0, i]
+    
+        return Y_prediction
+
+
+    def propagate(self, X, Y):
+        """
+        Arguments:
+        X -- data of size
+        Y -- true "label" vector (containing 0 and 1) of size (1, number of examples)
+
+        Return:
+        cost -- negative log-likelihood cost for logistic regression
+        dw -- gradient of the loss with respect to w, thus same shape as w
+        db -- gradient of the loss with respect to b, thus same shape as b
+        """
+        
+        m = X.shape[1]
+        
+        # compute activation
+        A = sigmoid(np.dot(self.w.T, X) + self.b) 
+
+        # compute cost
+        cost = -1 / m * np.sum(Y*np.log(A) + (1-Y)*np.log(1-A), axis = 1, keepdims = True)
+        
+        dw = 1 / m * np.dot(X, (A - Y).T)
+        db = 1 / m * np.sum(A - Y)
+        
+        cost = np.squeeze(np.array(cost))
+
+        grads = {
+            "dw": dw,
+            "db": db
+        }
+        
+        return grads, cost
+
+    def fit(self, X, Y):
+        """
+        Make fit for model object 
+        Return: 
+        Model with trained weights
+        """
+
+        self.initialize_weights(X.shape[0])
+
+        self.costs_log = []
+    
+        for _ in range(self.num_iterations):
+
+            # Retrieve derivatives from grads
+            grads, cost = self.propagate(X, Y)
+
+            self.costs_log.append(cost)
+            
+            self.w = self.w - self.learning_rate * grads["dw"] 
+            self.b = self.b - self.learning_rate * grads["db"]
+        
+        return self
 
 
 
@@ -263,7 +285,12 @@ if __name__ == '__main__':
     )
 
     X_train, X_test, Y_train, Y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
-    
-    model = model(X_train=X_train.T, Y_train=Y_train, X_test=X_test.T, Y_test=Y_test)
 
-    print(model)
+    X_train, X_test = X_train.T, X_test.T
+
+    model = LogisticRegression(learning_rate=0.1, num_iterations=10)
+
+    model.fit(X_train, Y_train)
+
+    print(accuracy_score(Y_test, model.predict(X_test)[0]))
+    print(roc_auc_score(Y_test, model.predict_proba(X_test)[0]))
